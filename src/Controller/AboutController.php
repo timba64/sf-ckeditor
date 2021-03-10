e57ee9336bfc3b55e0e136c969d069079b765d97
timba64 <timba64@mail.ru>
2021-03-10 10:55:36 +0300

    The About controller added and template

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class AboutController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/about", name="about")
      */
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
         ]);
     }
 }
